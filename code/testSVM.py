2de89c39fbad46393ded3ee8c3b2a2f9cd6407fa
OrZipori <or_zipori@walla.co.il>
2018-11-16 12:49:52 +0200

    Added utils.py for utility functions and classes (has SVM model in it) Added testSVM.py for testing the SVM and understanding the process of how to work with the data and transform it to binary

---
+++ code/testSVM.py
@@ -0,0 +1,33 @@
+# Test SVM model
+import utils
+import numpy as np 
+
+def main():
+    data = [(x, y) for x,y in utils.fetchData() if y in [0, 2]]
+    binaryData = utils.transformToBinaryClasses(data, positiveClass=0)
+
+    divider = int(round(.85 * len(binaryData)))
+    validation = binaryData[divider:]
+    trainset = binaryData[:divider]
+    inputDim = 784
+
+    model = utils.SVM(inputDim, utils.eta, 1, 10)
+    model.train(trainset)
+
+    # validate
+    correct = 0 
+    incorrect = 0
+    for x, y in validation:
+        y_tag = model.inference(x)
+        if (y == y_tag):
+            correct +=1
+        else:
+            incorrect +=1
+
+    acc = 1. * correct / len(validation)
+    print("correct: {} incorrect: {} total: {} \n accuracy: {}".format(\
+        correct, incorrect, len(validation), acc))
+    
+
+if __name__ == "__main__":
+    main()
--- code/utils.py
+++ code/utils.py
@@ -1,2 +1,70 @@
 import numpy as np 
+from sklearn.datasets import fetch_mldata
+from sklearn.utils import shuffle
 
+# global variables
+eta = 0.1
+
+def fetchData():
+    # read data
+    mnist = fetch_mldata("MNIST original", data_home="./data")
+    
+    X, Y = mnist.data[:60000] / 255., mnist.target[:60000]
+    x = [np.reshape(ex, (1, 784)) for ex, ey in zip(X, Y) if ey in [0, 1, 2, 3]]
+    y = [ey for ey in Y if ey in [0, 1, 2, 3]]
+    # suffle examples
+    x, y = shuffle(x, y, random_state=1)
+
+    return list(zip(x, y))
+
+'''
+ since we have multiclass data but wish to train binray classifier
+ we need to have a function that transforms classes to binray classification
+ parameter: data is a list of pairs of x, y where y had value only from 2 classes
+            positiveClass is the class that will be mapped to +1 all other will be
+                          mapped to -1
+'''
+def transformToBinaryClasses(data, positiveClass):
+    newData = []
+    for x, y in data:
+        if (y == positiveClass):
+            newData.append((x, 1))
+        else:
+            newData.append((x, -1))
+
+    return newData
+
+class SVM(object):
+    def __init__(self, inputDim, eta, lambdaP, epochs):
+        self.W = np.zeros((inputDim, 1)) # weights
+        self.eta = eta # learning rate
+        self.lambdaP = lambdaP # regularization parameter
+        self.epochs = epochs
+    
+    def train(self, trainset):
+        # iterate over epochs
+        for t in range(1, self.epochs + 1):
+            np.random.shuffle(trainset)
+            # we want to diminish eta the more we progress, to make "small steps", no to loose min
+            eta = self.eta / np.sqrt(t)
+            loss = 0
+
+            # iterate over trainset
+            for x, y in trainset:
+                pred = y * (np.dot(x, self.W)[0][0]) # get the scalar
+                reg = (eta * self.lambdaP) * self.W # regularization
+                if (1 - pred >= 0): # check hinge loss
+                    lossAddition = (eta * y * x)
+                    # need to reshape since x and W are not the same shape
+                    self.W += np.reshape(lossAddition, (784, 1)) - reg 
+                else:
+                    self.W -= reg
+                loss += 1 - pred
+            
+            print("Loss for #{} epoch is {}".format(t, loss))
+
+    def inference(self, x):
+        # since we use binary classifier
+        return np.sign(np.dot(x, self.W))[0][0]
+
+    
